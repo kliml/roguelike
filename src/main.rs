35163a59c708d64a2c0c73696291e804a732d7a4
Klim <klim.lestsenko@gmail.com>
2020-06-25 23:36:51 +0300

    menu system and invetory added

--- src/main.rs
+++ src/main.rs
@@ -28,6 +28,9 @@ const MSG_X: i32 = BAR_WIDTH + 2;
 const MSG_WIDTH: i32 = SCREEN_WIDTH - BAR_WIDTH - 2;
 const MSG_HEIGHT: usize = PANEL_HEIGHT as usize - 1;
 
+// Inevtory width
+const INVENTORY_WIDTH: i32 = 50;
+
 // FPS Limit
 const LIMIT_FPS: i32 = 20;
 
@@ -256,6 +259,14 @@ fn handle_keys(tcod: &mut Tcod, game: &mut Game, objects: &mut Vec<Object>) -> P
             }
             DidntTakeTurn
         }
+        (Key { code: Text, ..}, "i", true) => {
+            inventory_menu(
+                &game.inventory, 
+                "Press the key next to an item to use it, or any other to cancel.\n",
+                &mut tcod.root
+            );
+            TookTurn
+        }
 
         _ => DidntTakeTurn,
     }
@@ -271,6 +282,76 @@ fn get_names_under_mouse(mouse: Mouse, objects: &Vec<Object>, fov_map: &FovMap) 
         .collect::<Vec<_>>();
 
     names.join(", ")
+}
+
+pub fn menu<T: AsRef<str>>(header: &str, options: &Vec<T>, width: i32, root: &mut Root) -> Option<usize> {
+    assert!(
+        options.len() <= 26,
+        "Connot have a menu with more than 26 options."
+    );
+
+    // Calculate total height for the header and one line per option
+    let header_height = root.get_height_rect(0, 0, width, SCREEN_HEIGHT, header);
+    let height = options.len() as i32 + header_height;
+    
+    // Window menu    
+    let mut window = Offscreen::new(width, height);
+    window.set_default_foreground(WHITE);
+    window.print_rect_ex(
+        0,
+        0,
+        width,
+        height,
+        BackgroundFlag::None,
+        TextAlignment::Left,
+        header,
+    );
+    // Options
+    for (index, option_text) in options.iter().enumerate() {
+        let menu_letter = (b'a' + index as u8) as char;
+        let text = format!("({}) {}", menu_letter, option_text.as_ref());
+        window.print_ex(
+            0,
+            header_height + index as i32,
+            BackgroundFlag::None,
+            TextAlignment::Left,
+            text,
+        );
+    }
+
+    let x = SCREEN_WIDTH / 2 - width / 2;
+    let y = SCREEN_HEIGHT /2 - height / 2;
+    blit(&window, (0, 0), (width, height), root, (x, y), 1.0, 0.7);
+
+    root.flush();
+    let key = root.wait_for_keypress(true);
+
+    if key.printable.is_alphabetic() {
+        let index = key.printable.to_ascii_lowercase() as usize - 'a' as usize;
+        if index < options.len() {
+            Some(index)
+        } else {
+            None
+        }
+    } else {
+        None
+    }
+}
+
+pub fn inventory_menu(inventory: &Vec<Object>, header: &str, root: &mut Root) -> Option<usize> {
+    let options = if inventory.len() == 0 {
+        vec!["Invetory is empty.".into()]
+    } else {
+        inventory.iter().map(|item| item.name.clone()).collect()
+    };
+
+    let inventory_index = menu(header, &options, INVENTORY_WIDTH, root);
+
+    if inventory.len() > 0 {
+        inventory_index
+    } else {
+        None
+    }
 }
 
 fn main() {
